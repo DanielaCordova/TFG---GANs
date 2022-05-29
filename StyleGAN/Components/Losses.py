09fef0a3c540c992d9fecb9fbad413e1090fc4df
DanielaCordova <dancordo@ucm.es>
2022-05-29 04:34:00 +0200

    Update Losses.py

--- StyleGAN/Components/Losses.py
+++ StyleGAN/Components/Losses.py
@@ -1,94 +1,50 @@
-"""
--------------------------------------------------
-   File Name:    Losses.py
-   Author:       Zhonghao Huang
-   Date:         2019/10/21
-   Description:  Module implementing various loss functions
-                 Copy from: https://github.com/akanimax/pro_gan_pytorch
--------------------------------------------------
-"""
 
 import numpy as np
 import torch
 import torch.nn as nn
 from torch.nn import BCEWithLogitsLoss
 
-# =============================================================
-# Interface for the losses
-# =============================================================
+
 
 class GANLoss:
-    """ Base class for all losses
-
-        @args:
-        dis: Discriminator used for calculating the loss
-             Note this must be a part of the GAN framework
-    """
-
     def __init__(self, dis):
         self.dis = dis
 
     def dis_loss(self, real_samps, fake_samps, height, alpha):
-        """
-        calculate the discriminator loss using the following data
-        :param real_samps: batch of real samples
-        :param fake_samps: batch of generated (fake) samples
-        :param height: current height at which training is going on
-        :param alpha: current value of the fader alpha
-        :return: loss => calculated loss Tensor
-        """
+
         raise NotImplementedError("dis_loss method has not been implemented")
 
     def gen_loss(self, real_samps, fake_samps, height, alpha):
-        """
-        calculate the generator loss
-        :param real_samps: batch of real samples
-        :param fake_samps: batch of generated (fake) samples
-        :param height: current height at which training is going on
-        :param alpha: current value of the fader alpha
-        :return: loss => calculated loss Tensor
-        """
+
         raise NotImplementedError("gen_loss method has not been implemented")
 
 
 class ConditionalGANLoss:
-    """ Base class for all conditional losses """
 
     def __init__(self, dis):
         self.criterion = BCEWithLogitsLoss()
         self.dis = dis
 
-    def dis_loss(self, real_samps, fake_samps, labels, height, alpha):
-        # small assertion:
-        assert real_samps.device == fake_samps.device, \
-            "Real and Fake samples are not on the same device"
+    def dis_loss(self, realImgs, fakeImgs, labels, height, alpha):
 
-        # device for computations:
-        device = fake_samps.device
+        device = fakeImgs.device
 
-        # predictions for real images and fake images separately :
-        r_preds = self.dis(real_samps, height, alpha, labels_in=labels)
-        f_preds = self.dis(fake_samps, height, alpha, labels_in=labels)
+        # Predictions by Discriminator
+        real_preds = self.dis(realImgs, height, alpha, labels_in=labels)
+        fake_preds = self.dis(fakeImgs, height, alpha, labels_in=labels)
 
-        # calculate the real loss:
-        real_loss = self.criterion(
-            torch.squeeze(r_preds),
-            torch.ones(real_samps.shape[0]).to(device))
+        # Applies BCE in real
+        real_loss = self.criterion(torch.squeeze(real_preds), torch.ones(realImgs.shape[0]).to(device))
 
-        # calculate the fake loss:
-        fake_loss = self.criterion(
-            torch.squeeze(f_preds),
-            torch.zeros(fake_samps.shape[0]).to(device))
+        # Applies BCE in Fake
+        fake_loss = self.criterion(torch.squeeze(fake_preds), torch.zeros(fakeImgs.shape[0]).to(device))
 
-        # return final losses
+        #La media
         return (real_loss + fake_loss) / 2
 
-    def gen_loss(self, _, fake_samps, labels, height, alpha):
-        preds = self.dis(fake_samps, height, alpha, labels_in=labels)
-        return self.criterion(torch.squeeze(preds),
-                              torch.ones(fake_samps.shape[0]).to(fake_samps.device))
-
-
+    def gen_loss(self, _, fakeImgs, labels, height, alpha):
+        preds = self.dis(fakeImgs, height, alpha, labels_in=labels)
+        return self.criterion(torch.squeeze(preds), torch.ones(fakeImgs.shape[0]).to(fakeImgs.device)) ##Only BCE: predict Dis vs FakeImg
 
 
 class LogisticGAN(GANLoss):
@@ -97,35 +53,31 @@ class LogisticGAN(GANLoss):
 
     # gradient penalty
     def R1Penalty(self, real_img, height, alpha):
-
-        # TODO: use_loss_scaling, for fp16
-        apply_loss_scaling = lambda x: x * torch.exp(x * torch.Tensor([np.float32(np.log(2.0))]).to(real_img.device))
-        undo_loss_scaling = lambda x: x * torch.exp(-x * torch.Tensor([np.float32(np.log(2.0))]).to(real_img.device))
-
         real_img = torch.autograd.Variable(real_img, requires_grad=True)
+        ##Penalty only in Discriminator
         real_logit = self.dis(real_img, height, alpha)
-        # real_logit = apply_loss_scaling(torch.sum(real_logit))
+
         real_grads = torch.autograd.grad(outputs=real_logit, inputs=real_img,
                                          grad_outputs=torch.ones(real_logit.size()).to(real_img.device),
                                          create_graph=True, retain_graph=True)[0].view(real_img.size(0), -1)
-        # real_grads = undo_loss_scaling(real_grads)
+
         r1_penalty = torch.sum(torch.mul(real_grads, real_grads))
         return r1_penalty
 
     def dis_loss(self, real_samps, fake_samps, height, alpha, r1_gamma=10.0):
-        # Obtain predictions
-        r_preds = self.dis(real_samps, height, alpha)
-        f_preds = self.dis(fake_samps, height, alpha)
 
-        loss = torch.mean(nn.Softplus()(f_preds)) + torch.mean(nn.Softplus()(-r_preds))
+        real_preds = self.dis(real_samps, height, alpha)
+        fake_preds = self.dis(fake_samps, height, alpha)
 
-        if r1_gamma != 0.0:
+        loss = torch.mean(nn.Softplus()(fake_preds)) + torch.mean(nn.Softplus()(-real_preds))
+
+        if r1_gamma != 0.0: #Applies R1Penalty
             r1_penalty = self.R1Penalty(real_samps.detach(), height, alpha) * (r1_gamma * 0.5)
             loss += r1_penalty
 
         return loss
 
     def gen_loss(self, _, fake_samps, height, alpha):
-        f_preds = self.dis(fake_samps, height, alpha)
+        fake_preds = self.dis(fake_samps, height, alpha)
 
-        return torch.mean(nn.Softplus()(-f_preds))
+        return torch.mean(nn.Softplus()(-fake_preds))
